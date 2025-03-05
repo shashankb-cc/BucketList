988f0f002489fff99ebbb64fee38010fcccb638a
shashankb-cc <shashank.b@codecraft.co.in>
2025-03-05 10:53:45 +0530

    downloading data from wikipedia and sdding it to our screen

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -11,6 +11,13 @@ struct EditView: View {
     @Environment(\.dismiss) var dismiss
     var location: Location
     
+    enum LoadingState {
+        case loading, loaded, failed
+    }
+    
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
+    
     //we are using this in oreder to send the modoifioed tge location data object , since long press gives current location object and we modify it in editview , we have send back the edidted location object , but cant use binding it cant be done on options since selected location is optional we have to use onSave which return void in the closure of the content view we can get the new location object 
     var onSave: (Location) -> Void
 
@@ -33,6 +40,23 @@ struct EditView: View {
                     TextField("Place name", text: $name)
                     TextField("Description", text: $description)
                 }
+                
+                Section("Nearby…") {
+                    switch loadingState {
+                    case .loaded:
+                        ForEach(pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ") +
+                            Text(page.description)
+                                .italic()
+                        }
+                    case .loading:
+                        Text("Loading…")
+                    case .failed:
+                        Text("Please try again later.")
+                    }
+                }
             }
             .navigationTitle("Place details")
             .toolbar {
@@ -46,10 +70,35 @@ struct EditView: View {
                     dismiss()
                 }
             }
+            .task {
+                await fetchNearbyPlaces()
+            }
+        }
+    }
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            // we got some data back!
+            let items = try JSONDecoder().decode(Result.self, from: data)
+
+            // success – convert the array values to our pages array
+            pages = items.query.pages.values.sorted()
+            loadingState = .loaded
+        } catch {
+            // if we're still here it means the request failed somehow
+            loadingState = .failed
         }
     }
 }
 
-//#Preview {
-//    EditView(location: .example)
-//}
+#Preview {
+    EditView(location: .example, onSave: {_ in })
+}
